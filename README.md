8005c4bd202c29049eb4b5ba98d634d13f4e659c
luke0623 <luke0623@outlook.com>
2022-06-12 10:19:21 +0800

    add mysql script for quick start
    add mysql script for quick start

--- README.md
+++ README.md
@@ -7,5 +7,12 @@ docker compose -f docker-compose-mysql.yml up
 
 go to url localhost:3030
 
-login with super user imma-super/abc1234
+#### login with 
+- super user ：imma-super/change-me
+- admin user  ： imma-admin/1234abcd
+- console user ：imma-user/1234abcd
+
+
+
+
 
--- docker/mysql/init_user.sql
+++
@@ -1,5 +0,0 @@
-
-
-    insert into USERS (USERID, NAME, NICKNAME, PASSWORD, IS_ACTIVE, ROLE, CREATETIME, LASTMODIFIED, TENANTID)
-    values ('3', 'imma-super', null, '$2b$12$v5yHhyCW1sxNjZT9NKiIZuQU3mSaoC6CZkjJCgm.CD2sReTrBaOxO', '1', 'superadmin', null, now(), '1');
-
